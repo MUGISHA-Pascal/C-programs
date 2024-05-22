35ced4e4052d29688a967590266cd27374796d95
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-22 23:40:46 +0200

    C

---
+++ structures.c
@@ -0,0 +1,43 @@
+// #include<stdio.h>
+// declaring structures
+// struct {
+//     char *name;
+// }name1,name2;
+// int main(){
+// name1.name="mugisha";
+// name2.name="pascal";
+// printf("%s",name1.name);
+// printf("%s",name2.name);
+// return 0;
+// }
+
+// structures with function(external)
+// #include <stdio.h>
+// struct {
+//     char *name;
+//     int age;
+// }student1,student2;
+
+// int student3(){
+//     struct{
+//         char *name;
+//         int age;
+//     }student3;
+//     student3.name="kal";
+//     student3.age=30;
+//     if(student3.age>10){
+//         printf("age is perfect");
+//     }
+//     else{
+//         printf("the age is low");
+//     }
+//     return 0;
+// }
+// int main(){
+//     student1.age=23;
+//     printf("\t%d , %d",student1.age,student3());
+// return 0;
+// }
+
+
+
