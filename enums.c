f29bf24a7a688db7619a13a0a97f85f8c715c4aa
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-05-28 21:12:04 +0200

    C

---
+++ enums.c
@@ -0,0 +1,10 @@
+// //simple enum
+// #include<stdio.h>
+// enum numbers{num1=1,num2=2};
+// int main(){
+// enum numbers num;
+// num=num1;
+// printf("%d",num);
+//     return 0;
+// }
+
---
+++ filehandling.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    // //creating a file
+    // FILE *fpointer;
+    // fpointer=fopen("file1.txt","w");
+    // fclose(fpointer);
+    // return 0;
+
+//     //adding a character to a file
+//     FILE *fpointer;
+//     fpointer=fopen("file1.txt","w");
+//     int i;
+//     char text[25]="inputing a file";
+//     int length=strlen(text);
+//     for(i=0;i<length;i++){
+// fputc(text[i],fpointer);
+//     };
+//     fclose(fpointer);
+
+    return 0;
+}
