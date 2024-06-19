fcac9c516dba8a4a93e06e7af3e110e5d59a04c3
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-06-19 12:55:02 +0200

    C

---
+++ exam.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct _COMPLEX{
+    int first;
+    int second;
+    int third;
+}complex1;
+int main(){
+    struct _COMPLEX complex1;
+    int value1;
+    int value2;
+    printf("enter the first number : ");
+    scanf("%d",&value1);
+    complex1.first=value1;
+     printf("enter the second number : ");
+    scanf("%d",&value2);
+    complex1.first=value2;
+    complex1.third=value1+value2;
+    printf("the value of the sum is : %d ",complex1.third);
+    return 0;
+}
